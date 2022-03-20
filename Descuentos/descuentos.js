9d53a6ae15ba64221897e320b49c1db01e48a266
edwinj-ac <edwin.jesus.ac16@gmail.com>
2022-03-20 01:26:28 -0600

    Sistema de cupones

--- Descuentos/descuentos.js
+++ Descuentos/descuentos.js
@@ -11,6 +11,27 @@ console.log({
     precioConDescuento,
 });*/
 
+const coupons = [
+    "JuanDC_es_Batman",
+    "pero_no_le_digas_a_nadie",
+    "es_un_secreto",
+];
+
+const coupons_obj = [
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    },
+];
+
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -21,11 +42,32 @@ function onClickBtnPriceDiscount(){
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
 
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    let descuento;
+    if (!coupons.includes(couponValue)) {
+        alert("El cupón " + couponValue + "no es válido");
+    }
+
+    switch(couponValue) {
+        case coupons[0]: // "JuanDC_es_Batman"
+          descuento = 15;
+        break;
+        case coupons[1]: // "pero_no_le_digas_a_nadie"
+          descuento = 30;
+        break;
+        case coupons[2]: // "es_un_secreto"
+          descuento = 25;
+        break;
+        default:
+            descuento = 0;
+            break;
+      }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
     const resultP = document.getElementById("PriceResponse");
     resultP.innerText = "Precio con descuento: $" + precioConDescuento;
     
 }
+
